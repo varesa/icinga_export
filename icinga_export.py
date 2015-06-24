f1e05eb6854c1bf02c649a502a5ca3558d625e4e
Esa Varemo <esa@kuivanto.fi>
2015-06-24 22:00:20 +0300

    Do not show outdated checks that are no longer run

--- icinga_export.py
+++ icinga_export.py
@@ -9,10 +9,14 @@ dbc = mysql.connector.connect(user='icinga_read', password='Ax1sGW', host='192.1
 @app.route('/services')
 def view_services():
     cursor = dbc.cursor()
-    cursor.execute("SELECT icinga_hosts.display_name AS host_name, icinga_services.display_name as service_name, icinga_servicestatus.current_state as service_state "
+    cursor.execute("SELECT "
+                   "icinga_hosts.display_name AS host_name, "
+                   "icinga_services.display_name as service_name, "
+                   "icinga_servicestatus.current_state as service_state "
                    "FROM icinga_servicestatus "
                    "JOIN icinga_services ON icinga_servicestatus.service_object_id = icinga_services.service_object_id "
-                   "JOIN icinga_hosts ON icinga_services.host_object_id = icinga_hosts.host_object_id")
+                   "JOIN icinga_hosts ON icinga_services.host_object_id = icinga_hosts.host_object_id "
+                   "WHERE icinga_servicestatus.status_update_time >= DATE_SUB(NOW(), INTERVAL 1 HOUR)")
     i_host_name = cursor.column_names.index("host_name")
     i_service_name = cursor.column_names.index("service_name")
     i_service_state = cursor.column_names.index("service_state")
